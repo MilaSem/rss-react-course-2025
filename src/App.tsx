6984f780d3a81b55be2bfe73a48216caa42e2b02
MilaSem <milafoxsem@gmail.com>
2025-07-11 19:14:36 +0300

    feat: implement core logic in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { Component } from 'react';
 
+import { fetchAnimeBySearchTerm } from './api/fetchAnimeBySearchTerm';
+import { fetchPopularAnime } from './api/fetchPopularAnime';
+
+import type { Media } from './types/anilistTypes';
+import { SearchPanel } from './components/SearchPanel/SearchPanel';
+import { Results } from './components/Results/Results';
+
+import styles from './App.module.css';
+
 interface AppState {
+  items: Media[];
+  loading: boolean;
+  error: string | null;
   triggerError: boolean;
 }
 
@@ -8,18 +20,61 @@ export class App extends Component<Record<string, never>, AppState> {
   constructor(props: Record<string, never>) {
     super(props);
     this.state = {
+      items: [],
+      loading: false,
+      error: null,
       triggerError: false,
     };
   }
+
+  fetchItems = async (searchTerm: string) => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      let response;
+      if (searchTerm) {
+        response = await fetchAnimeBySearchTerm(searchTerm);
+      } else {
+        response = await fetchPopularAnime();
+      }
+      this.setState({ items: response.data.Page.media, loading: false });
+    } catch (error: unknown) {
+      let message = '';
+
+      if (error instanceof Error) {
+        message = error.message;
+      }
+
+      this.setState({
+        error: message,
+        loading: false,
+      });
+    }
+  };
+
+  handleSearch = (searchTerm: string) => {
+    void this.fetchItems(searchTerm);
+  };
 
   render() {
     if (this.state.triggerError) {
       throw new Error('Test error from render');
     }
+
     return (
       <>
-        <h1>Anime Catalog</h1>
-        <button onClick={() => this.setState({ triggerError: true })}>
+        <h1 className={styles.title}>Anime Catalog</h1>
+        <SearchPanel onSearch={this.handleSearch} />
+        <Results
+          loading={this.state.loading}
+          error={this.state.error}
+          items={this.state.items}
+        />
+
+        <button
+          className={styles.error}
+          onClick={() => this.setState({ triggerError: true })}
+        >
           Error
         </button>
       </>
