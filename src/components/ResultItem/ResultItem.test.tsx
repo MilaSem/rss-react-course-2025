091987084c455c41be1026afe10872fc33adc274
MilaSem <milafoxsem@gmail.com>
2025-07-20 10:20:30 +0300

    feat: add tests for result item component

--- src/components/ResultItem/ResultItem.test.tsx
+++ src/components/ResultItem/ResultItem.test.tsx
@@ -1,7 +1,11 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { ResultItem } from './ResultItem';
-import { mockMediaItem1 } from '@/test-utils/mockMedia';
+import {
+  mockMediaItem1,
+  mockMediaItem2,
+  mockMediaItem3,
+} from '@/test-utils/mockMedia';
 
 describe('ResultItem', () => {
   it('should display title and description correctly', () => {
@@ -16,14 +20,31 @@ describe('ResultItem', () => {
     expect(description).toBeInTheDocument();
   });
 
+  it('should display romaji title if english title is missing', () => {
+    const mockItemWithoutEnglishTitle = mockMediaItem3;
+    render(<ResultItem item={mockItemWithoutEnglishTitle} />);
+    expect(screen.getByText('Romaji Title 3')).toBeInTheDocument();
+  });
+
   it('should cope with missing description property', () => {
-    const mockItemWithoutDescription = {
-      ...mockMediaItem1,
-      description: undefined,
-    };
+    const mockItemWithoutDescription = mockMediaItem2;
     render(<ResultItem item={mockItemWithoutDescription} />);
     expect(
       screen.getByText('No description for this item'),
     ).toBeInTheDocument();
+  });
+
+  it('should display alt text if no title is available', () => {
+    const item = {
+      ...mockMediaItem3,
+      title: {
+        english: undefined,
+        romaji: undefined,
+      },
+    };
+    render(<ResultItem item={item} />);
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('alt', 'Poster');
   });
 });
