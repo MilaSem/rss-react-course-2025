94e37831b6fa45916fbcac983bbcc26dbb720f3a
MilaSem <milafoxsem@gmail.com>
2025-07-27 02:00:55 +0300

    refactor: convert result item from class to function

--- src/components/ResultItem/ResultItem.tsx
+++ src/components/ResultItem/ResultItem.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import type { Media } from '@/types/anilistTypes';
 
 import styles from './ResultItem.module.css';
@@ -10,27 +9,23 @@ interface ResultItemProps {
   item: Media;
 }
 
-export class ResultItem extends Component<ResultItemProps> {
-  render() {
-    const { item } = this.props;
-
-    return (
-      <div className={styles.item}>
-        <p className={styles.title}>
-          {item.title.english || item.title.romaji || 'Title not available'}
-        </p>
-        <p className={styles.description}>
-          {cleanAndTrimText(MAX_LENGTH, item.description) ||
-            'No description for this item'}
-        </p>
-        {item.coverImage?.extraLarge && (
-          <img
-            src={item.coverImage.extraLarge}
-            alt={item.title.english || item.title.romaji || 'Poster'}
-            className={styles.poster}
-          />
-        )}
-      </div>
-    );
-  }
-}
+export const ResultItem = ({ item }: ResultItemProps) => {
+  return (
+    <div className={styles.item}>
+      <p className={styles.title}>
+        {item.title.english || item.title.romaji || 'Title not available'}
+      </p>
+      <p className={styles.description}>
+        {cleanAndTrimText(MAX_LENGTH, item.description) ||
+          'No description for this item'}
+      </p>
+      {item.coverImage?.extraLarge && (
+        <img
+          src={item.coverImage.extraLarge}
+          alt={item.title.english || item.title.romaji || 'Poster'}
+          className={styles.poster}
+        />
+      )}
+    </div>
+  );
+};
