9c7e818d5f1015755e68ff47e4151612580312b6
MilaSem <milafoxsem@gmail.com>
2025-07-26 16:48:47 +0300

    refactor: convert spinner from class to function

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import styles from './Spinner.module.css';
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <div
-        className={styles.spinner}
-        aria-label="Loading"
-        aria-busy="true"
-        data-testid="spinner"
-      ></div>
-    );
-  }
-}
+export const Spinner = () => {
+  return (
+    <div
+      className={styles.spinner}
+      aria-label="Loading"
+      aria-busy="true"
+      data-testid="spinner"
+    ></div>
+  );
+};
